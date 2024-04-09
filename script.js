818815fcc5012d55d3877c954c55ad83fe1be126
Hom Dahal <homdahal997@gmail.com>
2024-04-09 15:41:58 -0400

    Part two started

--- script.js
+++ script.js
@@ -34,3 +34,21 @@ console.log(`The remainder of dividing the result by the fourth number is ${rema
 
 // Part 1: Math Problems - End
 //-----------------------------------------------------------------------------------------
+
+
+
+//Part 2: Practical Math- start
+//-----------------------------------------------------------------------------------------
+/**
+Let’s look at a more practical scenario.
+You are planning a cross-country road trip!
+The distance of the trip, in total, is 1,500 miles.
+Your car’s fuel efficiency is as follows:
+At 55 miles per hour, you get 30 miles per gallon.
+At 60 miles per hour, you get 28 miles per gallon.
+At 75 miles per hour, you get 23 miles per gallon.
+You have a fuel budget of $175.
+The average cost of fuel is $3 per gallon.
+Set up a program to answer the following questions:
+ */
+
